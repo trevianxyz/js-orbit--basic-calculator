a6496cf7b5d3be5d4c105925ba9c010556ea5d31
trevianxyz <37381924+trevianxyz@users.noreply.github.com>
2020-01-09 05:34:32 -0500

    calc

---
+++ script.js
@@ -0,0 +1,16 @@
+let x = "value1";
+let y = "value2";
+
+function times(x, y) {
+return(x * y);
+}
+
+function division(x, y) {
+return(x / y);
+}
+
+if (times)
+console.log();
+
+if (division)
+console.log();
